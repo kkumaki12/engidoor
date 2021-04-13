efff7b975be3a0afeb2f3722448cef292ed53cd2
kkumaki12 <79786417+kkumaki12@users.noreply.github.com>
2021-04-13 19:12:50 +0900

    ログイン、ログアウト機能実装 #1

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get 'questions/create'
   get 'questions/edit'
   get '/search', to:'questions#list'
+  get '/login', to:'sessions#new'
+  post '/login', to:'sessions#create'
+  delete '/logout', to:'sessions#destroy'
  
 
   resources :users
