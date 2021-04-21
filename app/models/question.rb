ca50c188054315459ed675f11894e64290f22da8
kkumaki12 <79786417+kkumaki12@users.noreply.github.com>
2021-04-21 19:30:12 +0900

    新着質問を新しいものが上に来るように修正 #16

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,5 @@ class Question < ApplicationRecord
   validates :user_id, presence: true
   validates :title, presence: true, length: { maximum: 30 }
   validates :content, presence: true, length: { maximum: 2000 }
+  default_scope -> { order(created_at: :desc) }
 end
