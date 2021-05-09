1a62d73085b91a454c00e0f48e63abd06081c2ae
kkumaki12 <79786417+kkumaki12@users.noreply.github.com>
2021-05-09 19:49:23 +0900

    テスト追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,11 @@ RSpec.describe User, type: :model do
   it 'ユーザー名がない場合、無効である' do
     expect(FactoryBot.build(:user, name: '')).to_not be_valid
   end
+
+  it 'メールアドレスがない場合、無効である' do
+    expect(FactoryBot.build(:user, email: '')).to_not be_valid
+  end
+
+  it 'ログイン中のユーザーと一致しない場合、編集ができない' do
+  end
 end
