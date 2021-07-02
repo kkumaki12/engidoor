bbdd0200b8e76b3e69ada3ce99aefd1fb3addf5f
kkumaki12 <79786417+kkumaki12@users.noreply.github.com>
2021-07-03 00:03:15 +0900

    [add]axios導入

--- app/controllers/api/v1/best_answers_controller.rb
+++ app/controllers/api/v1/best_answers_controller.rb
@@ -1,1 +1,10 @@
 class Api::V1::BestAnswersController < ApiController
+
+  def create
+    comment = Comment.find(params[:comment_id])
+    question_id = comment.question_id
+    @best_answer = BestAnswer.create(question_id: question_id, comment_id: params[:comment_id])
+    @best_answer.save
+    redirect_to root_path
+end
+end
