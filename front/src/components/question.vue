3dce76330007b607913178caf6a37bf66f39853e
kkumaki12 <79786417+kkumaki12@users.noreply.github.com>
2021-07-03 19:20:35 +0900

    [add]frontにtailwind導入

--- front/src/components/question.vue
+++ front/src/components/question.vue
@@ -8,14 +8,16 @@
       <label class="block text-gray-600 text-sm font-semibold mb-2" for="title">
         タイトル
       </label>
-      <input v-model="postTitle" class="bg-gray-100 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+      <input v-model="postTitle" type="text" class="bg-gray-100 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
 
     </div>
     <div class="mb-4">
       <label class="block text-gray-600 text-sm font-semibold mb-2" for="tag">
         タグ
       </label>
-       <input v-model="postTag" placeholder= "複数タグ付けする場合はスペースを開けてください" class= "bg-gray-100 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="tag">
+       <input v-model="postTag" 
+       placeholder= "複数タグ付けする場合はスペースを開けてください" 
+       class= "bg-gray-100 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="tag">
     </div>
     <div class="mb-4">
       <label
@@ -24,11 +26,13 @@
       >
         内容
       </label>
-      <textarea v-model="postContent" class="bg-gray-100 p-1 appearance-none border rounded w-full text-gray-700 leading-tight focus:outline-none focus:shadow-outline" rows="8"></textarea>
+      <textarea v-model="postContent"
+       class="bg-gray-100 p-1 appearance-none border rounded w-full text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+       rows="8"></textarea>
     </div>
     
     <div class="flex items-center justify-between">
-        <button @click="postQuestion()" class= "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">投稿する</button>
+        <button @click="createQuestion()" class= "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">投稿する</button>
     </div>
   </div>
 </div>
@@ -37,14 +41,29 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
 
   data: function () {
     return {
-      postTitle: "",
-      postTag: "",
-      postContent: "",
+      question: {
+      postTitle: '',
+      postTag: '',
+      postContent: '',
+      },
     };
   },
+  methods: {
+    createQuestion(){
+      axios.post('/api/v1/questions/create', this.question)
+      .then(response => {
+        console.log(response);
+
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    }
+  }
 };
 </script>
--- front/src/router.js
+++ front/src/router.js
@@ -2,12 +2,13 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue'
 import Users from './views/Users.vue'
+import QuestionForm from './components/question.vue'
 
 
 Vue.use(Router);
 
 export default new Router({
   mode: "history",
-  routes: [{path: '/', component: Home },{path: '/users', component: Users}]
+  routes: [{path: '/', component: Home },{path: '/users', component: Users},{path: '/question', component: QuestionForm}]
 });
 
