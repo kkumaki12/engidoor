0410fcc4fd5b35c06c3665f249be2184add8acc3
kkumaki12 <79786417+kkumaki12@users.noreply.github.com>
2021-09-03 19:08:19 +0900

    [fix]返信が即時反映されるよう修正#141

--- front/src/components/Comments/CommentsReplys.vue
+++ front/src/components/Comments/CommentsReplys.vue
@@ -123,6 +123,7 @@ export default {
         })
         .then((response) => {
           console.log(response);
+          this.replyCatch();
         })
         .catch((error) => {
           console.log(error);
