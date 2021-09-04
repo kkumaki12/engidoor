5592de32a0bca30c5f7a3ecd35cd008c6e5d6316
kkumaki12 <79786417+kkumaki12@users.noreply.github.com>
2021-09-04 11:16:12 +0900

    [fix]carrierwave修正#143

--- back/config/initializers/carrierwave.rb
+++ back/config/initializers/carrierwave.rb
@@ -8,13 +8,13 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_directory = 'www.engidoor.com'
     config.fog_public = false
-    #config.fog_credentials = {
-     # provider: 'AWS',
-     # aws_access_key_id: Rails.application.credentials.aws_access_key_id,
-     # aws_secret_access_key: Rails.application.credentials.aws_secret_access_key,
-     # region: 'ap-northeast-1',
-     # path_style: true
-    #}
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      region: 'ap-northeast-1',
+      path_style: true
+    }
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/www.engidoor.com'
   else
     config.storage :file
